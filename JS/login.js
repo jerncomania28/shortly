699079359f8151adf489d09814b1745a6989dab2
jerncomania28 <okonnduesojeremiah@gmail.com>
2022-03-30 23:17:31 +0100

    finished

--- JS/login.js
+++ JS/login.js
@@ -4,6 +4,38 @@ const signIn = document.querySelector('#signIn');
 const formContainer = document.querySelector('.form-container');
 
 
+
+let users = [
+    {
+        name: "Bathelomew",
+        email: "bathelomew345@gmail.com",
+        country: "Nigeria",
+        phone: 09076908330,
+        password: "emmanuel"
+    },
+    {
+        name: "Valero",
+        email: "valero35@gmail.com",
+        country: "Nigeria",
+        phone: 08034673456,
+        password: "valero"
+    },
+    {
+        name: "Matthew",
+        email: "mattt345@gmail.com",
+        country: "Nigeria",
+        phone: "07078274234",
+        password: "math"
+    }
+];
+
+
+let mail = users.map(item => item.email);
+
+let pass = users.map(item => item.password);
+
+
+
 signIn.addEventListener('click', checkInput);
 
 
@@ -11,15 +43,17 @@ const paragraph = document.createElement('p');
 
 function checkInput(event) {
 
-    if (email.value === "" || password.value === "") {
+    if (!mail.includes(email.value) && !pass.includes(password.value)) {
 
         paragraph.innerText = "make sure the fields are not empty";
 
-        paragraph.classList.add('error'); formContainer.appendChild(paragraph);
+        paragraph.classList.add('error'); 
+        formContainer.appendChild(paragraph);
 
         event.preventDefault();
 
     }
+    formContainer.removeChild(paragraph);
 }
-formContainer.removeChild(paragraph);
+
 
